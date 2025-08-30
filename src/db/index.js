f8e2d3ad65fae945e2b6ed68ff208ce962bee528
HarshalBhosle <harsalb1202@gmail.com>
2025-08-30 14:04:15 +0530

    apis

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         console.log(`\n MongoDB connected !! DB host :${connectionInstance.connection.host}`);
-        
+          
     } catch (error) {
         console.log("MONGODB connection error", error);
         process.exit(1)
